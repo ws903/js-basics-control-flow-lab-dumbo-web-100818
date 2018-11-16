0097f32c02bd90df364b68ad8125b877449d3215
ws903 <ws903@nyu.edu>
2018-11-16 16:13:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet) {
+
+	let result;
+
+	if(feet <= 400) {
+		result = 'This one is on me!';
+	} else if(feet > 2500) {
+		result = 'No can do.';
+	} else {
+		result = 'I will gladly take your thirty bucks.';
+	}
+
+	return result;
+
+}
+
+function ternaryCheckCity(city) {
+	let result;
+
+	switch(city) {
+		case 'NYC':
+			result = 'Ok, sounds good.';
+			break;
+		default:
+			result = 'No go.';
+			break;
+	}
+
+	return result
+}
+
+function switchOnCharmFromTip(tip) {
+	let result;
+	switch(tip) {
+		case 'generous':
+			result = 'Thank you so much.'
+			break;
+		case 'not as generous':
+			result = 'Thank you.'
+			break;
+		default:
+			result = 'Bye.'
+			break;
+	}
+
+	return result
+}
